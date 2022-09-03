3ad36661d13b1eadb6f42b4f459aef487adce772
senvr <leeeyecard@gmail.com>
2022-09-02 19:43:24 -0700

    0.1

--- bot.py
+++ bot.py
@@ -1,30 +1,24 @@
+import asyncio
+import os
+
+import discord
+import logging
+from discord.ext import tasks, commands
 from dotenv import load_dotenv
-import discord, logging
-from discord.ext import commands
-import psutil, os, sys
-from discord_slash import SlashCommand
 
 
-class Bot(commands.AutoShardedBot):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.ver = os.getenv("DISCORD_BOT_VERSION")
-        self.process = psutil.Process(os.getpid())
-        self.bot_module = sys.modules[__name__]
-        self.slash = SlashCommand(self, sync_commands=True)
+class SenvrBot(commands.AutoShardedBot):
+    def __init__(self, prefix: str = "$"):
+        super().__init__(
+            commands.when_mentioned_or(prefix),
+            intents=discord.Intents.all(),
+            case_insensitive=True,
+            # help_command=None
+        )
+        self.status_queue = asyncio.LifoQueue(maxsize=8)
 
-    @commands.Cog.listener()
-    async def on_connect(self):
-        logging.info(f"Connected. Loading extensions...")
-        await self.load_all_cogs()
-
-    @commands.Cog.listener()
-    async def on_ready(self):
-        logging.info(f"Version: {self.ver}\nProcess: {self.process}")
-
-
-    async def load_all_cogs(self):
-        load_dotenv()
+    def bot_reload(self):
+        print(f"Loading modules")
         for filename in os.listdir(os.getenv("DISCORD_COGS_PATH")):
             if filename.endswith(".py"):
                 module_name = filename[:-3]
@@ -33,18 +27,26 @@ class Bot(commands.AutoShardedBot):
                     logging.info(f"Reloading existing module")
                     self.reload_extension(module_name)
                 else:
-                    logging.info(f"Loading extension {module_name}")
                     self.load_extension(module_path)
+                logging.info(f"{module_name} loaded")
 
+    @tasks.loop(seconds=1)
+    async def status_updater(self):
+        if not self.status_queue.empty():
+            await bot.change_presence(
+                activity=discord.Activity(type=discord.ActivityType.watching, name=str(await self.status_queue.get())))
+
+
+bot = SenvrBot()
 if __name__ == "__main__":
-    load_dotenv()
-    print(f"Logging bot as main application...")
-    logging.basicConfig(level=logging.INFO)
-    bot = Bot(
-        commands.when_mentioned_or("$"),
-        intents=discord.Intents.all(),
-        case_insensitive=True,
-        help_command=None
-    )
-    bot.run(os.getenv("DISCORD_TOKEN"), reconnect=True)
+    @bot.event
+    async def on_connect():
+        bot.bot_reload()
+        bot.status_updater.start()
+        print(f'We have logged in as {bot.user}')
 
+
+    load_dotenv()
+    logging.getLogger().setLevel(logging.DEBUG)
+    logging.getLogger("discord").setLevel(logging.WARNING)
+    bot.run(os.getenv("DISCORD_TOKEN"), reconnect=True)
--- cogs/MarkovifyLive.py
+++ cogs/MarkovifyLive.py
@@ -1,69 +1,124 @@
-import functools
-from discord.ext import commands, tasks
-from dotenv import load_dotenv
 import asyncio
-import discord, logging
-import os, markovify, pickle
+import concurrent.futures
 
-
-class UserModel:
-    def __init__(self, user: discord.Member, bot):
-        self.bot = bot
-        self.user = user
-        self.model_access_lock = asyncio.Lock()
-        self.model = None
-        self.training_tasks = []
-        self.input_queue = asyncio.Queue()
-
-    async def add_message(self, message: discord.Message):
-        return await self.input_queue.put(message)
-
-    async def get_model(self):
-        async with self.model_access_lock:
-            logging.info(f"{self.user} model: waiting for model to finish training")
-            if len(self.training_tasks) > 0:
-                task = self.training_tasks[0]
-                logging.debug(f"Waiting on task {task}")
-                if self.model:
-                    logging.debug(f"{self.user} model: combining")
-                    self.model = markovify.combine(self.model, await task)
-                else:
-                    logging.debug(f"{self.user} model: setting")
-                    self.model = await task
-            return self.model
-
-
-    @tasks.loop()
-    async def process_input_queue(self):
-        sentences_sample = ""
-        if self.input_queue.qsize() > int(os.getenv("ML_MIN_SENTENCES")):
-            async with self.model_access_lock:
-                logging.info(f"{self.user} model: now training")
-                while not self.input_queue.empty():
-                    message = await self.input_queue.get()
-                    try:
-                        sentences_sample += f"{message}\n"
-                    finally:
-                        self.input_queue.task_done()
-                logging.debug(f"{self.user} model: combined samples, starting training")
-                self.training_tasks.append(self.bot.loop.run_in_executor(None, functools.partial(markovify.NewlineText,
-                                                                                                 data={
-                                                                                                     'input_text': sentences_sample,
-                                                                                                     'well_formed': False
-                                                                                                 })))
+import markovify
+import os
+import html
+import discord
+import logging
+from discord.ext import commands
+import time
+from dotenv import load_dotenv
+import bot
+from discord.ext import tasks, commands
 
 
 class MarkovifyLive(commands.Cog):
-    def __init__(self, bot):
+    def __init__(self, bot: bot.SenvrBot):
         self.bot = bot
+        self.crawlers = []
+        self.model = None
+        self.lock = asyncio.Lock()
+        self.samples = asyncio.Queue()
+        self.counter = 0
+        self.train_on_queue.start()
+        self.diversity = 0
+        self.executor = concurrent.futures.ThreadPoolExecutor(max_workers=int(os.getenv("ML_TASKS")))
+        self.messages_per_sec = 0
+        self.last_messages_per_sec = 0
+        self.rate_change = 0
+        self.status_poster.start()
+
+    @tasks.loop(seconds=2)
+    async def status_poster(self):
+        self.rate_change = ((self.messages_per_sec - self.last_messages_per_sec) + self.rate_change) / 2
+        self.last_messages_per_sec = self.messages_per_sec
+        if self.rate_change > 0:
+            await self.bot.status_queue.put(f"{round(self.messages_per_sec)}/s {round(self.rate_change)} ↑")
+        if self.rate_change < 0:
+            await self.bot.status_queue.put(f"{round(self.messages_per_sec)}/s {round(self.rate_change)} ↓")
+
+    @tasks.loop(seconds=10)
+    async def train_on_queue(self):
+        sample = ""
+        start_time = time.time()
+        counter = 0
+        while not self.samples.empty():
+            message = await self.samples.get()
+            sample += message.content.strip() + "\n"
+            self.samples.task_done()
+            counter += 1
+        self.counter = self.counter + counter
+        if sample:
+            try:
+                logging.info(f"training on sample")
+                new_model = await self.bot.loop.run_in_executor(self.executor, markovify.NewlineText,
+                                                                (html.escape(sample)))
+                async with self.lock:
+                    if self.model:
+                        logging.debug(f"combining models")
+                        self.model = await self.bot.loop.run_in_executor(self.executor, markovify.combine,
+                                                                         (self.model, new_model))
+                    else:
+                        logging.debug(f"new model")
+                        self.model = new_model
+            except KeyError as e:
+                logging.warning(f"{sample} caused {e}")
+
+        end_time = time.time()
+        time_taken = (end_time - start_time)
+        messages_per_second = counter / time_taken
+        self.messages_per_sec = (messages_per_second + self.messages_per_sec) / 2
+
+    async def channel_crawler(self, channel: discord.TextChannel):
+        logging.info(f"{channel}: Crawling now...")
+        try:
+            async for message in channel.history(oldest_first=True, limit=None):
+                if self.samples.qsize() >= int(os.getenv("ML_SAMPLE_SIZE")):
+                    logging.info(f"Hit max ML_SAMPLE_SIZE")
+                    logging.debug(f"{channel}: Training on {self.samples.qsize()} samples...")
+                    await self.train_on_queue()
+                    logging.debug(f"{channel}: Done")
+                await self.samples.put(message)
+        except discord.Forbidden as e:
+            logging.warning(f"{channel}: {e}")
+        logging.debug(f"{channel}: finishing queue")
+        await self.train_on_queue()
+
+    async def wait_on_crawlers(self):
+        logging.info(f"Waiting on {len(self.crawlers)} crawlers")
+        for task_done in asyncio.as_completed(self.crawlers):
+            logging.debug(f"{task_done}: waiting")
+            await task_done
+            logging.debug(f"{task_done} done")
 
     @commands.Cog.listener()
     async def on_ready(self):
-        await self.bot.close()
+        for guild in self.bot.guilds:
+            for channel in guild.text_channels:
+                task = asyncio.create_task(self.channel_crawler(channel))
+                if len(self.crawlers) >= int(os.getenv('ML_TASKS')):
+                    logging.info(f"Hit max ML_TASKS")
+                    await self.wait_on_crawlers()
+                self.crawlers.append(task)
+        logging.info(f"Waiting for crawlers to finish")
+        await self.wait_on_crawlers()
+        logging.info(f"Finishing rest of queue")
+        await self.train_on_queue()
+        logging.info(f"All caught up")
 
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        await self.input_queue.put(message)
+    @commands.command()
+    async def speak(self, ctx):
+        async with ctx.typing():
+            async with self.lock:
+                if self.model:
+                    result = await self.bot.loop.run_in_executor(self.executor, self.model.make_sentence)
+                    if result:
+                        await ctx.reply(html.unescape(result))
+                    else:
+                        await ctx.reply("?")
+                else:
+                    await ctx.reply("No accessable model yet, please wait")
 
 
 def setup(bot):
