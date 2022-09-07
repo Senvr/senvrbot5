ecd8abf7d15730ae5c75699e7b616f7ce23ea724
senvr <leeeyecard@gmail.com>
2022-09-06 23:37:19 -0700

    0.57

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@ set -e
 sudo apt update
 
 sudo apt install python3 python3-pip python3-venv -y
-python3 -m . senvrbot
-source .bin/activate
+sudo python3 -m venv .
+source ./bin/activate
 python3 --version
 python3 -m pip install -r requirements.txt
 deactivate
