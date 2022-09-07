21e18b87f86c158882ed52ecb2fb73b78752fcc8
senvr <leeeyecard@gmail.com>
2022-09-06 23:59:14 -0700

    0.573

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -xe
-sudo apt update
-sudo apt install python3 python3-pip python3-venv -y
+sudo apt-get update
+sudo apt-get install python3 python3-pip python3-venv -y
 python3 -m venv .
 source ./bin/activate
 python3 --version
